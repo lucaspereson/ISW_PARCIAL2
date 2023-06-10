75ef15d6e045000bf693a0159e37595decb01a3a
Lucas Pereson <lucaspereson17@gmail.com>
2023-06-10 13:07:48 -0300

    36-viendo que onda

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,17 @@
-console.log("Hola mundo");
+// console.log("Hola mundo");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('¡Hola, mundo!');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
+
+
 
 let square = function(number) {
     return number * 2;
