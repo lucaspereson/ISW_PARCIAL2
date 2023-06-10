0a837f61c69a51b4924dd65e89046e8eb92ed5d4
Lucas Pereson <lucaspereson17@gmail.com>
2023-06-10 15:07:44 -0300

    48

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,8 @@
 let square = function(number) {
     return number * number;
   }
-document.getElementById("resultado").textContent = "PARCIAL  2 - ISW";
-
+//document.getElementById("resultado").textContent = "PARCIAL  2 - ISW";
+console.log('hola mundo');
 module.exports = {
     square
 }
