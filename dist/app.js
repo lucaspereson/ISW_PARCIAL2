3caa0ac6bf6b363becd245911f9f98f768a1292d
Lucas Pereson <lucaspereson17@gmail.com>
2023-06-10 20:29:17 -0300

    84

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,7 @@
 const { JSDOM } = require('jsdom');
 const fs = require('fs');
 
-const html = fs.readFileSync('index.html', 'utf-8');
+const html = fs.readFileSync('./dist/index.html', 'utf-8');
 const dom = new JSDOM(html);
 const document = dom.window.document;
 
