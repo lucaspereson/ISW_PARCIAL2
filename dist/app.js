46ebe251e29ac3ca781be67ba2cf5351827f69cb
Lucas Pereson <lucaspereson17@gmail.com>
2023-06-10 15:23:18 -0300

    51

--- dist/app.js
+++ dist/app.js
@@ -2,7 +2,7 @@ let square = function(number) {
     return number * number;
   }
 //document.getElementById("resultado").textContent = "PARCIAL  2 - ISW";
-console.log('hola mundo');
+
 module.exports = {
     square
 }
