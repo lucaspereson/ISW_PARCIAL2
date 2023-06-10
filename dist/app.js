1556192998ec5b02759e5a07f723f53904e84ee7
Lucas Pereson <lucaspereson17@gmail.com>
2023-06-10 20:27:42 -0300

    83

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,17 @@
+const { JSDOM } = require('jsdom');
+const fs = require('fs');
+
+const html = fs.readFileSync('index.html', 'utf-8');
+const dom = new JSDOM(html);
+const document = dom.window.document;
+
+// Aquí puedes utilizar el objeto `document` para manipular el DOM
+const resultadoElemento1 = document.getElementById("resultado1");
+resultadoElemento1.textContent = square(3);
+
 let square = function(number) {
   return number * number;
 };
-
 
 module.exports = {
     square
