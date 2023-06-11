b2210f10c664081b9d2bf261a2a55c50d15e601b
Lucas Pereson <lucaspereson17@gmail.com>
2023-06-11 10:19:52 -0300

    92

--- app1.test.js
+++ app1.test.js
@@ -1,6 +1,7 @@
 const app = require('./dist/app');
 const notif = require('./app.feedback');
+import square from ('./dist/app');
 
 test("TEST 1 (3)", () => {
-  expect(app.square(3)).toBe(9);
+  expect(square(3)).toBe(9);
 });
