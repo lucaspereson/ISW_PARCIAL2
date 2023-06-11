1031983a3f05b4ca7de79f56ac44490c048fceab
Lucas Pereson <lucaspereson17@gmail.com>
2023-06-11 10:53:13 -0300

    97

--- app1.test.js
+++ app1.test.js
@@ -2,7 +2,7 @@ const app = require('./dist/app');
 const notif = require('./app.feedback');
 
 test("TEST 1 (3)", () => {
-  if (app.square(3) !== 9 ) {
+  if (app.cuadrado(3) !== 9 ) {
     notif.enviarNotificacionAutor('Test 1 Fallido: La operacion no produjo el resultado esperado');
   };
   const valor = app.cuadrado(3);
--- app2.test.js
+++ app2.test.js
@@ -2,7 +2,7 @@ const app = require('./dist/app');
 const notif = require('./app.feedback');
 
 test("TEST 2 (8)", () => {
-  if (app.square(8) !== 64 ) {
+  if (app.cuadrado(8) !== 64 ) {
     notif.enviarNotificacionAutor('Test 2 Fallido: La operacion no produjo el resultado esperado');
   };
   const valor = app.cuadrado(8);
