dd918d8c1e47ef538036e3e513086a08e5f991e4
Lucas Pereson <lucaspereson17@gmail.com>
2023-06-12 20:26:57 -0300

    128

--- src/app.js
+++ src/app.js
@@ -7,49 +7,49 @@ module.exports = {
 function cuadrado2(number){
   return number * number;
 };
-function cuadrado2(number){
+function cuadrado3(number){
   return number * number;
 };
-function cuadrado2(number){
+function cuadrado4(number){
   return number * number;
 };
-function cuadrado2(number){
+function cuadrado5(number){
   return number * number;
 };
-function cuadrado2(number){
+function cuadrado6(number){
   return number * number;
 };
-function cuadrado2(number){
+function cuadrado7(number){
   return number * number;
 };
-function cuadrado2(number){
+function cuadrado8(number){
   return number * number;
 };
-function cuadrado2(number){
+function cuadrado9(number){
   return number * number;
 };
-function cuadrado2(number){
+function cuadrado210(number){
   return number * number;
 };
-function cuadrado2(number){
+function cuadrado11(number){
   return number * number;
 };
-function cuadrado2(number){
+function cuadrado12(number){
   return number * number;
 };
-function cuadrado2(number){
+function cuadrado13(number){
   return number * number;
 };
-function cuadrado2(number){
+function cuadrado14(number){
   return number * number;
 };
-function cuadrado2(number){
+function cuadrado15(number){
   return number * number;
 };
-function cuadrado2(number){
+function cuadrado16(number){
   return number * number;
 };
-function cuadrado2(number){
+function cuadrado17(number){
   return number * number;
 };
 
