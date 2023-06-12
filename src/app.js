da9700ef6c6de0cf56e91629948ee035b31a94b8
Lucas Pereson <lucaspereson17@gmail.com>
2023-06-12 18:28:51 -0300

    120

--- src/app.js
+++ src/app.js
@@ -6,18 +6,6 @@ module.exports = {
   cuadrado
 }
 
-const { JSDOM } = require('jsdom');
-const fs = require('fs');
-
-const html = fs.readFileSync('./src/index.html', 'utf-8');
-const dom = new JSDOM(html);
-const document = dom.window.document;
-
-// Aquí puedes utilizar el objeto `document` para manipular el DOM
-const resultadoElemento1 = document.getElementById("resultado1");
-resultadoElemento1.innerHTML = '3^2 = ' + cuadrado(3);
-const resultadoElemento2 = document.getElementById("resultado2");
-resultadoElemento2.innerHTML = '8^2 = ' + cuadrado(8);
 
 
 
