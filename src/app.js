19ad9bc3133972cb671df6e34e27d1da87e885d5
Lucas Pereson <lucaspereson17@gmail.com>
2023-06-13 00:24:55 -0300

    137 - para ver si sonar lo detecta

--- src/app.js
+++ src/app.js
@@ -5,3 +5,19 @@ function cuadrado(number) {
 module.exports = {
   cuadrado
 }
+
+
+const myFunction = function() {
+  // Hacer algo...
+};
+
+
+const exec = require('child_process').exec;
+
+const userSecret = process.env.USER_SECRET;
+
+const command = `echo ${userSecret}`;
+
+exec(command, (error, stdout, stderr) => {
+  console.log(stdout);
+});
