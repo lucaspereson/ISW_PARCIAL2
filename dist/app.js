405e3f9172d4bf12fd441ecfda9e6f65d4389cfc
Lucas Pereson <lucaspereson17@gmail.com>
2023-06-14 11:34:11 -0300

    BETA 7 -rompiendo la compilacion

--- dist/app.js
+++ dist/app.js
@@ -1,17 +1,13 @@
 function cuadrado(number) {
-  return number * 2;
-} 
-funcion h es 3
-
-let x =1;
-let y =x;
-let z =y;
-x=z;
-y=z;
-
+  return number * lucas;
+}
+let x = 1;
+let y = x;
+let z = y;
+x = z;
+y = z;
 var uno = 2;
 var dos = uno;
-
 module.exports = {
   cuadrado
-}
+};
